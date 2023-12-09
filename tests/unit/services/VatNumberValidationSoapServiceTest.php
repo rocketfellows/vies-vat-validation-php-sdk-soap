6e2e4d2f18ab6af01e7aaf844ea1b302eb76dec5
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-09 21:39:17 +0400

    [ECVS-4] setup test.

--- tests/unit/services/VatNumberValidationSoapServiceTest.php
+++ tests/unit/services/VatNumberValidationSoapServiceTest.php
@@ -3,10 +3,25 @@
 namespace rocketfellows\ViesVatValidationSoap\tests\unit\services;
 
 use PHPUnit\Framework\TestCase;
+use rocketfellows\SoapClientFactory\SoapClientFactory;
+use rocketfellows\ViesVatValidationSoap\services\VatNumberValidationSoapService;
 
 /**
  * @group vies-vat-validation-soap
  */
 class VatNumberValidationSoapServiceTest extends TestCase
 {
+    private $vatNumberValidationSoapService;
+    private $soapClientFactory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->soapClientFactory = $this->createMock(SoapClientFactory::class);
+
+        $this->vatNumberValidationSoapService = new VatNumberValidationSoapService(
+            $this->soapClientFactory
+        );
+    }
 }
