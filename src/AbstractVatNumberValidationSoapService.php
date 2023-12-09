257561215870ce2188520b3f1fae22501b3d620a
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-09 20:31:05 +0400

    [ECVS-4] added implementation stub.

--- src/AbstractVatNumberValidationSoapService.php
+++ src/AbstractVatNumberValidationSoapService.php
@@ -2,6 +2,27 @@
 
 namespace rocketfellows\ViesVatValidationSoap;
 
-class AbstractVatNumberValidationSoapService
+use rocketfellows\ViesVatValidationInterface\exceptions\service\GlobalMaxConcurrentReqServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\GlobalMaxConcurrentReqTimeServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidInputServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidRequesterInfoServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\IPBlockedServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\MSMaxConcurrentReqServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\MSMaxConcurrentReqTimeServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\MSUnavailableServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\ServiceRequestException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\ServiceUnavailableException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\TimeoutServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\UnknownServiceErrorException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\VatBlockedServiceException;
+use rocketfellows\ViesVatValidationInterface\VatNumber;
+use rocketfellows\ViesVatValidationInterface\VatNumberValidationResult;
+use rocketfellows\ViesVatValidationInterface\VatNumberValidationServiceInterface;
+
+class AbstractVatNumberValidationSoapService implements VatNumberValidationServiceInterface
 {
+    public function validateVat(VatNumber $vatNumber): VatNumberValidationResult
+    {
+        // TODO: Implement validateVat() method.
+    }
 }
