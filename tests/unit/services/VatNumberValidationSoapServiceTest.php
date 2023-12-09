866f7c009543d4007155e82c25ad7ba20c9301da
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-09 21:38:07 +0400

    [ECVS-4] created vat number validation soap service test.

---
+++ tests/unit/services/VatNumberValidationSoapServiceTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace rocketfellows\ViesVatValidationSoap\tests\unit\services;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group vies-vat-validation-soap
+ */
+class VatNumberValidationSoapServiceTest extends TestCase
+{
+}
