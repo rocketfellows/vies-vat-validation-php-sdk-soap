91dd5fbc6268b692dfdcc14560053042b2ccbe12
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-09 21:31:26 +0400

    [ECVS-4] implemented vat number validation soap service.

--- src/services/VatNumberValidationSoapService.php
+++ src/services/VatNumberValidationSoapService.php
@@ -6,10 +6,8 @@ use rocketfellows\ViesVatValidationSoap\AbstractVatNumberValidationSoapService;
 
 class VatNumberValidationSoapService extends AbstractVatNumberValidationSoapService
 {
-    private const WSDL_SOURCE = 'https://ec.europa.eu/taxation_customs/vies/services/checkVatService.wsdl';
-
     protected function getWsdlSource(): string
     {
-        return self::WSDL_SOURCE;
+        return 'https://ec.europa.eu/taxation_customs/vies/services/checkVatService.wsdl';
     }
 }
