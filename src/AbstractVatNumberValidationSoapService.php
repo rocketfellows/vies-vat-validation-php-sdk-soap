01d9115237808e408f24d32498e3605d8f1556f5
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-23 16:42:01 +0400

    [ECVS-4] fixed.

--- src/AbstractVatNumberValidationSoapService.php
+++ src/AbstractVatNumberValidationSoapService.php
@@ -4,19 +4,7 @@ namespace rocketfellows\ViesVatValidationSoap;
 
 use Exception;
 use rocketfellows\SoapClientFactory\SoapClientFactory;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\GlobalMaxConcurrentReqServiceException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\GlobalMaxConcurrentReqTimeServiceException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidInputServiceException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidRequesterInfoServiceException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\IPBlockedServiceException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\MSMaxConcurrentReqServiceException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\MSMaxConcurrentReqTimeServiceException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\MSUnavailableServiceException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\ServiceRequestException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\ServiceUnavailableException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\TimeoutServiceException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\UnknownServiceErrorException;
-use rocketfellows\ViesVatValidationInterface\exceptions\service\VatBlockedServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\ServiceRequestException;
 use rocketfellows\ViesVatValidationInterface\FaultCodeExceptionFactory;
 use rocketfellows\ViesVatValidationInterface\VatNumber;
 use rocketfellows\ViesVatValidationInterface\VatNumberValidationResult;
@@ -26,18 +14,6 @@ use stdClass;
 
 abstract class AbstractVatNumberValidationSoapService implements VatNumberValidationServiceInterface
 {
-    private const SOAP_FAULT_CODE_INVALID_INPUT = 'INVALID_INPUT';
-    private const SOAP_FAULT_CODE_SERVICE_UNAVAILABLE = 'SERVICE_UNAVAILABLE';
-    private const SOAP_FAULT_CODE_MS_UNAVAILABLE = 'MS_UNAVAILABLE';
-    private const SOAP_FAULT_CODE_TIMEOUT = 'TIMEOUT';
-    private const SOAP_FAULT_CODE_INVALID_REQUESTER_INFO = 'INVALID_REQUESTER_INFO';
-    private const SOAP_FAULT_CODE_VAT_BLOCKED = 'VAT_BLOCKED';
-    private const SOAP_FAULT_CODE_IP_BLOCKED = 'IP_BLOCKED';
-    private const SOAP_FAULT_CODE_GLOBAL_MAX_CONCURRENT_REQ = 'GLOBAL_MAX_CONCURRENT_REQ';
-    private const SOAP_FAULT_CODE_GLOBAL_MAX_CONCURRENT_REQ_TIME = 'GLOBAL_MAX_CONCURRENT_REQ_TIME';
-    private const SOAP_FAULT_CODE_MS_MAX_CONCURRENT_REQ = 'MS_MAX_CONCURRENT_REQ';
-    private const SOAP_FAULT_CODE_MS_MAX_CONCURRENT_REQ_TIME = 'MS_MAX_CONCURRENT_REQ_TIME';
-
     private $faultCodeExceptionFactory;
     private $soapClientFactory;
 
@@ -82,31 +58,6 @@ abstract class AbstractVatNumberValidationSoapService implements VatNumberValida
 
     private function handleSoapFault(SoapFault $fault): Exception
     {
-        switch ($fault->getMessage()) {
-            case self::SOAP_FAULT_CODE_INVALID_INPUT:
-                return new InvalidInputServiceException($fault->getMessage(), $fault->getCode(), $fault);
-            case self::SOAP_FAULT_CODE_SERVICE_UNAVAILABLE:
-                return new ServiceUnavailableException($fault->getMessage(), $fault->getCode(), $fault);
-            case self::SOAP_FAULT_CODE_MS_UNAVAILABLE:
-                return new MSUnavailableServiceException($fault->getMessage(), $fault->getCode(), $fault);
-            case self::SOAP_FAULT_CODE_TIMEOUT:
-                return new TimeoutServiceException($fault->getMessage(), $fault->getCode(), $fault);
-            case self::SOAP_FAULT_CODE_INVALID_REQUESTER_INFO:
-                return new InvalidRequesterInfoServiceException($fault->getMessage(), $fault->getCode(), $fault);
-            case self::SOAP_FAULT_CODE_VAT_BLOCKED:
-                return new VatBlockedServiceException($fault->getMessage(), $fault->getCode(), $fault);
-            case self::SOAP_FAULT_CODE_IP_BLOCKED:
-                return new IPBlockedServiceException($fault->getMessage(), $fault->getCode(), $fault);
-            case self::SOAP_FAULT_CODE_GLOBAL_MAX_CONCURRENT_REQ:
-                return new GlobalMaxConcurrentReqServiceException($fault->getMessage(), $fault->getCode(), $fault);
-            case self::SOAP_FAULT_CODE_GLOBAL_MAX_CONCURRENT_REQ_TIME:
-                return new GlobalMaxConcurrentReqTimeServiceException($fault->getMessage(), $fault->getCode(), $fault);
-            case self::SOAP_FAULT_CODE_MS_MAX_CONCURRENT_REQ:
-                return new MSMaxConcurrentReqServiceException($fault->getMessage(), $fault->getCode(), $fault);
-            case self::SOAP_FAULT_CODE_MS_MAX_CONCURRENT_REQ_TIME:
-                return new MSMaxConcurrentReqTimeServiceException($fault->getMessage(), $fault->getCode(), $fault);
-            default:
-                return new UnknownServiceErrorException($fault->getMessage(), $fault->getCode(), $fault);
-        }
+        return $this->faultCodeExceptionFactory->create($fault->getMessage(), $fault->getMessage());
     }
 }
